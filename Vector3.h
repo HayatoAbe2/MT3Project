e6a2b7c7049d6162c3eb2f327ac8537d3ff12a19
HayatoAbe2 <k024g0033@g.neec.ac.jp>
2025-04-10 18:10:22 +0900

    3次元ベクトルの基本的な計算の関数

---
+++ Vector3.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <math.h>
+
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+
+Vector3 Add(const Vector3& v1, const Vector3& v2) {
+	Vector3 result;
+	result.x = v1.x + v2.x;
+	result.y = v1.y + v2.y;
+	result.z = v1.z + v2.z;
+	return result;
+}
+
+Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
+	Vector3 result;
+	result.x = v1.x - v2.x;
+	result.y = v1.y - v2.y;
+	result.z = v1.z - v2.z;
+	return result;
+}
+
+Vector3 Multiply(float scalar, const Vector3& v) {
+	Vector3 result;
+	result.x = scalar * v.x;
+	result.y = scalar * v.y;
+	result.z = scalar * v.z;
+	return result;
+}
+
+float Dot(const Vector3& v1, const Vector3& v2) {
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+float Length(const Vector3& v) {
+	return sqrtf(Dot(v, v));
+}
+
+Vector3 Normalize(const Vector3& v) {
+	Vector3 result;
+	result.x = v.x / Length(v);
+	result.y = v.y / Length(v);
+	result.z = v.z / Length(v);
+	return result;
+}
