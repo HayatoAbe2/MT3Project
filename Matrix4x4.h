cd2a1206834d695a813329a2b410dd702f9ee4d3
HayatoAbe2 <k024g0033@g.neec.ac.jp>
2025-04-17 14:23:16 +0900

    拡縮・平行移動の行列を三次元に拡張(00_03)

--- Matrix4x4.h
+++ Matrix4x4.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "Vector3.h"
+#include <cassert>
 
 struct Matrix4x4 {
 	float m[4][4];
@@ -138,7 +140,44 @@ Matrix4x4 Transpose(const Matrix4x4& m) {
 
 // 単位行列
 Matrix4x4 MakeIdentity4x4() {
-	Matrix4x4 result = { 0 };
+	Matrix4x4 result = {};
+	for (int i = 0; i < 4; ++i) {
+		result.m[i][i] = 1.0f;
+	}
+	return result;
+};
+
+
+// 拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+	Matrix4x4 result = {};
+	result.m[0][0] = scale.x;
+	result.m[1][1] = scale.y;
+	result.m[2][2] = scale.z;
+	result.m[3][3] = 1.0f;
+	return result;
+};
+
+// 座標変換
+Vector3 Transform(const Vector3& vector, const Matrix4x4 matrix) {
+	Vector3 result;
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + matrix.m[3][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + matrix.m[3][1];
+	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + matrix.m[3][2];
+	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + matrix.m[3][3];
+	assert(w != 0.0f);
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+	return result;
+};
+
+// 平行移動行列
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
+	Matrix4x4 result = {};
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
 	for (int i = 0; i < 4; ++i) {
 		result.m[i][i] = 1.0f;
 	}
